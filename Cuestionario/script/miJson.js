810d7b2a6e316791601bfedd640f49aaa16069bb
Saul <sauloteromelchor@gmail.com>
2022-11-07 11:01:09 +0100

    principio del html

--- Cuestionario/script/miJson.js
+++ Cuestionario/script/miJson.js
@@ -5,12 +5,31 @@ let preguntas = [
     //que a su vez tiene otro objeto
     "pregunta":[
             {
+                "modo":"simple",
                 "pregunta":"¿En que videojuego aparece The Ashen One?",
-                "respuestas":["Dark Souls","Dark Souls 2", "Among Us", "Dark Souls 3"]
+                "respuestas":[{
+                    "respuesta":"Dark Souls","correcta":""},
+                    {"respuesta":"Dark Souls 2","correcta":""},
+                    {"respuesta":"Among Us","correcta":""},
+                    {"respuesta":"Dark Souls 3","correcta":"true"}]
             },
             {
+                "modo":"simple",
                 "pregunta":"¿Cual es el mejor videojuego de 2017?",
-                "respuestas":["TloZ: Botw","Persona 5", "NieR: Automata", "Yakuza 0"]
+                "respuestas":[{
+                    "respuesta":"TloZ: Botw","correcta":""},
+                    {"respuesta":"Persona 5","correcta":"true"},
+                    {"respuesta":"NieR: Automata","correcta":""},
+                    {"respuesta":"Yakuza 0","correcta":""}]
+            },
+            {
+                "modo":"multi",
+                "pregunta":"¿En que videojocs aparece Super Mario?",
+                "respuestas":[{
+                    "respuesta":"TloZ: Botw","correcta":""},
+                    {"respuesta":"Super Mario Land","correcta":"true"},
+                    {"respuesta":"Smash Bros","correcta":"true"},
+                    {"respuesta":"Mario Party","correcta":"true"}]
             }
         ]
 },
@@ -18,16 +37,34 @@ let preguntas = [
     "categoria":"anime",
     "pregunta":[
             {
+                "modo":"simple",
                 "pregunta":"¿Quien dice la inspiradora frase Tatakae?",
-                "respuestas":["Armin","Perry el Ornitorrinco","Ereh", "Zac Effron"]
+                "respuestas":[{
+                    "respuesta":"Armin","correcta":""},
+                    {"respuesta":"Perry el Ornitorrinco","correcta":""},
+                    {"respuesta":"Ereh","correcta":"true"},
+                    {"respuesta":"Zac Effron","correcta":""}]
             },
             {
+                "modo":"simple",
                 "pregunta":"¿Cual es el mejor anime ever?",
-                "respuestas":["FMA: Brotherhood","Shingeki no Kyojin","Kimetsu no Yaiba", "Pingu"]
+                "respuestas":[{"respuesta":"FMA: Brotherhood","correcta":"true"},
+                {"respuesta":"Shingeki no Kyojin","correcta":""},
+                {"respuesta":"Kimetsu no Yaiba","correcta":""},
+                {"respuesta":"Pingu","correcta":""}]
+            },
+            {
+                "modo":"multi",
+                "pregunta":"¿Cuales de estos animes esta animado por Mappa?",
+                "respuestas":[{"respuesta":"FMA: Brotherhood","correcta":""},
+                {"respuesta":"Shingeki no Kyojin s4","correcta":""},
+                {"respuesta":"Kimetsu no Yaiba","correcta":""},
+                {"respuesta":"Danse Danse Danceur","correcta":"true"}]
             }
         ]
 }
 ]
+<<<<<<< Updated upstream
 //tenemos que iterar tres veces porque tenemos 2 objetos y un array
 for (let pregunta of preguntas){
     console.log("Categoria "+pregunta.categoria);
@@ -39,3 +76,33 @@ for (let pregunta of preguntas){
    }
     
 }
+||||||| constructed merge base
+
+
+
+
+for (let pregunta of preguntas){
+    console.log("Categoria "+pregunta.categoria);
+   for (let categoria of pregunta.pregunta){
+    console.log(categoria.pregunta)
+    for (let respuesta of categoria.respuestas){
+        console.log(respuesta);
+    }        
+   }
+    
+}
+=======
+
+
+
+
+// for (let pregunta of preguntas){
+//     console.log("Categoria "+pregunta.categoria);
+//    for (let categoria of pregunta.pregunta){
+//     console.log(categoria.pregunta)
+//     for (let respuesta of categoria.respuestas){
+//         console.log(respuesta);
+//     }        
+//    }    
+// }
+>>>>>>> Stashed changes
