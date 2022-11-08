45d042d3f19161aba615b0e5f66525ea59b63dc4
Saul <sauloteromelchor@gmail.com>
2022-11-08 08:15:20 +0100

    preguntas completas

--- Cuestionario/script/miJson.js
+++ Cuestionario/script/miJson.js
@@ -48,6 +48,38 @@ let preguntas = [
                     {"respuesta":"PolyStation","correcta":""},
                     {"respuesta":"MexBox","correcta":""},
                     {"respuesta":"NES","correcta":"true"}]
+            },
+            {
+                "modo":"simple",
+                "pregunta":"¿Con que nombre se le conoce ahora al famoso streamer Alberkiller?",
+                "respuestas":[{
+                    "respuesta":"AlberHealer","correcta":""},
+                    {"respuesta":"Albersou","correcta":"true"},
+                    {"respuesta":"Alber2","correcta":""},
+                    {"respuesta":"JordiWildENP","correcta":""}]
+            },
+            {
+                "modo":"simple",
+                "pregunta":"¿Para jugar Fallout 76 es necesario jugar a los 75 anteriores?",
+                "respuestas":[{
+                    "respuesta":"Si","correcta":""},
+                    {"respuesta":"No","correcta":"true"}]
+            },
+            {
+                "modo":"simple",
+                "pregunta":"¿Se deberia considerar personas a los jugadores de FIFA/CoD?",
+                "respuestas":[{
+                    "respuesta":"Si","correcta":""},
+                    {"respuesta":"No","correcta":"true"}]
+            },
+            {
+                "modo":"multi",
+                "pregunta":"Seleccione el genero del videojuego Inazuma Eleven",
+                "respuestas":[{
+                    "respuesta":"Accion","correcta":""},
+                    {"respuesta":"Furbo","correcta":"true"},
+                    {"respuesta":"RPG","correcta":"true"},
+                    {"respuesta":"No Binario","correcta":""}]
             }
         ]
 },
@@ -78,6 +110,62 @@ let preguntas = [
                 {"respuesta":"Shingeki no Kyojin s4","correcta":""},
                 {"respuesta":"Kimetsu no Yaiba","correcta":""},
                 {"respuesta":"Danse Danse Danceur","correcta":"true"}]
+            },
+            {
+                "modo":"simple",
+                "pregunta":"¿Cual de estos animes trata de una familia de espias?",
+                "respuestas":[{
+                    "respuesta":"Detective Conan","correcta":""},
+                    {"respuesta":"Lupin the 3rd","correcta":""},
+                    {"respuesta":"Katekyo hitman reborn","correcta":""},
+                    {"respuesta":"Spy x Family","correcta":"true"}]
+            },
+            {
+                "modo":"simple",
+                "pregunta":"¿Cual de estas series tiene un final que no corresponde con el original?",
+                "respuestas":[{
+                    "respuesta":"Full Metal Alchemist","correcta":"true"},
+                    {"respuesta":"Full Metal Alchemist: Brotherhood","correcta":""}]
+            },
+            {
+                "modo":"multi",
+                "pregunta":"¿Seleccione los mangas cuyos autores sean mujeres?",
+                "respuestas":[{"respuesta":"FMA: Brotherhood","correcta":"true"},
+                {"respuesta":"Gintama","correcta":""},
+                {"respuesta":"Kimetsu no Yaiba","correcta":"true"},
+                {"respuesta":"Inuyasha","correcta":"true"}]
+            },
+            {
+                "modo":"simple",
+                "pregunta":"¿Cual de estas series se desarrolla en el espacio?",
+                "respuestas":[{"respuesta":"Cowboy Beebop","correcta":"true"},
+                {"respuesta":"Neon Geneis Evangelion","correcta":""},
+                {"respuesta":"Berserk","correcta":""},
+                {"respuesta":"Vinland Saga","correcta":""}]
+            },
+            {
+                "modo":"multi",
+                "pregunta":"¿Cual o cuales de estas series estan siendo emitidas en Otoño 2022?",
+                "respuestas":[{"respuesta":"Your Lie in April","correcta":""},
+                {"respuesta":"Chainsaw Man","correcta":"true"},
+                {"respuesta":"Spy x Family","correcta":"true"},
+                {"respuesta":"Bobobo","correcta":""}]
+            },
+            {
+                "modo":"multi",
+                "pregunta":"¿Que series tienen como genero Juegos?",
+                "respuestas":[{"respuesta":"Yu-gi-oh","correcta":"true"},
+                {"respuesta":"3-Gatsu no Lion","correcta":"true"},
+                {"respuesta":"Yakusoku no Neverland","correcta":""},
+                {"respuesta":"Kakegurui","correcta":"true"}]
+            },
+            {
+                "modo":"simple",
+                "pregunta":"¿Con que ataque mata Gohan a Celula?",
+                "respuestas":[{"respuesta":"Big Bang Attack","correcta":""},
+                {"respuesta":"Kamehameha","correcta":""},
+                {"respuesta":"Kamehameha Padre-hijo","correcta":"true"},
+                {"respuesta":"Allargat Basto","correcta":""}]
             }
         ]
 }
